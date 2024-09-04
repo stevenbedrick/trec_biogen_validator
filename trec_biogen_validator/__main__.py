1fce1187f9e31c4626f78834749eba80d19f72f0
Steven Bedrick <bedricks@ohsu.edu>
2024-09-03 21:14:57 -0700

    Added option to enable/disable sentence tokenization dumping.

--- trec_biogen_validator/__main__.py
+++ trec_biogen_validator/__main__.py
@@ -23,6 +23,7 @@ def cmd(
     max_sentences_per_output: int = DEFAULT_MAX_SENTENCES_PER_ANSWER,
     max_words_per_output: int = DEFAULT_MAX_WORDS_PER_ANSWER,
     spacy_model: str = DEFAULT_SPACY_MODEL,
+    dump_sentence_tokenization: bool = False,
 ):
     """
     Perform validation of a TREC Biogen submission, according to the rules described
@@ -34,6 +35,7 @@ def cmd(
     :param max_sentences_per_output: Maximum number of sentences per answer
     :param max_words_per_output:
     :param spacy_model:
+    :param dump_sentence_tokenization: If true, print out the sentence tokenization for topics that have errors or warnings, to help with debugging.
     :return:
     """
 
@@ -82,7 +84,9 @@ def cmd(
             for err_type, msg in validation_results.warnings:
                 console.print(f"\t\t⚠️ {msg}")
 
-        if len(validation_results.errors) > 0 or len(validation_results.warnings) > 0:
+        if (
+            len(validation_results.errors) > 0 or len(validation_results.warnings) > 0
+        ) and dump_sentence_tokenization:
             console.print("\tSentence-level tokenization, in case it helps:")
             for sentence_idx, parsed_sentence in enumerate(
                 validation_results.parsed_answer.sentences
